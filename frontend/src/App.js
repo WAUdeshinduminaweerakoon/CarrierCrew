42dac077eca8012dce5669300cfabdf367fbfa41
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-07-31 22:00:07 +0530

    creact the project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a>
+        Carrier Crew  
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

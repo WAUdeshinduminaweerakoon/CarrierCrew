fe2d8edf03daeec34f97811e4d66f4a3b039d67f
AsankaRukshan <rathnay-se19053@stu.kln.ac.lk>
2024-12-01 12:28:28 +0530

    Modified the folder structure with login and home

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-3xl font-bold text-green-800">
+        Welcome to the Home Page!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;

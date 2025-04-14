87546465de6aa425953136074a4b22643f576ff9
AsankaRukshan <rathnay-se19053@stu.kln.ac.lk>
2025-04-14 07:43:55 +0530

    Added employer home page and job seeker home page

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./components/login/Login";
 import Home from "./components/jobSeeker/Home";
+import EmployerHome from "./components/employer/Home";
 
 // Create router with routes
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <Home />,
+  },
+  {
+    path: "/employer/home",
+    element: <EmployerHome />,
   },
 ]);
 

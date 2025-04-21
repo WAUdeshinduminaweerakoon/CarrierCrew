b1853edcdbff759f9426e96f6a2a01b6ff64ef7d
AsankaRukshan <rathnay-se19053@stu.kln.ac.lk>
2025-04-21 12:05:04 +0530

    Modified file location

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // import Login from "./components/Login";
 // import Home from "./components/Home";
-import AddEmployerDetails from "./components/employer/Employer";
+import AddEmployerDetails from "./components/login/Employer";
 import AddCompanyDetails from "./components/employer/CompanyDetails";
 import AddJobPost from "./components/jobpost/CreateJobPost";
 

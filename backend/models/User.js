045b0b4fb0aa9121c4c48f23848f6d666e3b5e73
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2025-05-29 22:19:29 +0530

    add the User.js file

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    userType: { type: String, enum: ['JobSeeker', 'Employer'], required: true },
+});
+
+// Compare password method
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);

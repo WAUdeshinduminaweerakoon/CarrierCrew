16beda93041d7f5f972d9d0d32ced87957d072bd
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2025-05-30 00:29:08 +0530

    add the nodemon

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,8 +4,27 @@ const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'Password must be at least 8 characters'],
+    },
     userType: { type: String, enum: ['JobSeeker', 'Employer'], required: true },
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
 });
 
 // Compare password method

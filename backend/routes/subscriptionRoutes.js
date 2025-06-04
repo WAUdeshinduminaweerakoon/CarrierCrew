b3e71e72312f6c769ea9edee32e77cfc37037204
AsankaRukshan <rathnay-se19053@stu.kln.ac.lk>
2025-06-04 22:12:44 +0530

    Backend functionalities related to activate, renew and change subscription plans

--- backend/routes/subscriptionRoutes.js
+++ backend/routes/subscriptionRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const {createSubscriptionPlan, getAllSubscriptionPlans, updateSubscriptionPlan,assignSubscriptionPlan,getSubscriptionPlanByName } = require('../controllers/subscriptionController');
+const { activateSubscription, renewSubscription, getSubscriptionsByEmployerId, changeSubscriptionPlan } = require('../controllers/subscriptionController');
+
+
 //const { assignSubscriptionPlan } = require('../controllers/subscriptionController');
 const router = express.Router();
 
@@ -16,6 +19,24 @@ router.post('/assign-plan', assignSubscriptionPlan);
 
 // GET : Get Subscription plan by name
 router.get('/subscription-plan/:planName', getSubscriptionPlanByName);
+
+// activate new subscription
+router.post('/activate', activateSubscription);
+
+//renew subscription
+router.post('/renew', renewSubscription);
+
+//get subscription details for a particular user
+router.get('/employer/:employerId', getSubscriptionsByEmployerId);
+
+//change subscription
+router.post('/change', changeSubscriptionPlan);
+
+
+
+
+
+
 
 
 

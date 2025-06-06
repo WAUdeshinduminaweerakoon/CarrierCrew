34c77203b98627b28d0cd79983d3136334284f29
AsankaRukshan <rathnay-se19053@stu.kln.ac.lk>
2025-06-07 02:16:46 +0530

    resolved issue

--- backend/routes/otpRoutes.js
+++ backend/routes/otpRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { sendOtp, verifyOtp } = require('../controllers/otpController');
+const { sendOtpHandler, verifyOtp } = require('../controllers/otpController');
 const router = express.Router();
 
-router.post("/send-otp", sendOtp);
+router.post("/send-otp", sendOtpHandler);
 router.post("/verify-otp", verifyOtp);
 
 module.exports = router;

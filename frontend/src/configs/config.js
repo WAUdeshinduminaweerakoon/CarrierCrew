be621ab30a518284d35ab4b220ca3b799a254704
AsankaRukshan <rathnay-se19053@stu.kln.ac.lk>
2025-06-08 03:36:26 +0530

    File uploading backend

--- frontend/src/configs/config.js
+++ frontend/src/configs/config.js
@@ -12,6 +12,7 @@ const API_ROUTES = {
   CATEGORY : `${BASE_URL}/api/category`,
   PAYMENT : `${PAYMENT_GATEWAY}/api/payment`,
   ACCOUNT :  `${BASE_URL}/api/auth/account`,
+  UPLOAD :  `${BASE_URL}/api/upload`,
 };
 
 export default API_ROUTES;
